fef05c5aa7f072cdcf4d6b0a932fbd5d754e71d7
Ilya  Stepanov <6967221@gmail.com>
2022-11-04 14:56:08 +0100

    reverseList

---
+++ easy/reverseList.js
@@ -0,0 +1,10 @@
+const reverseList = function(head) {
+    let prev = null;
+    while (head) {
+        const next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+};
